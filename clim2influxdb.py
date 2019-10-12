c893700c209fdac12cb19469e0f7323c7eac6c27
Ludovic Rousseau <ludovic.rousseau@free.fr>
2019-10-12 17:04:56 +0200

    clim2influxdb: add support of day and month

--- clim2influxdb.py
+++ clim2influxdb.py
@@ -3,6 +3,9 @@
 """Get air conditioning data from Panasonic cloud
 
 Display the data for the current date and time
+
+Exemple:
+  Clim consigne=28.000000,inside=23
 """
 
 import pcomfortcloud
@@ -12,6 +15,10 @@ import sys
 import logging
 import logging.handlers
 import pprint
+import datetime
+
+LOGIN = os.environ['CONFORT_CLOUD_LOGIN']
+PASSWORD = os.environ['CONFORT_CLOUD_PASSWORD']
 
 logger = logging.getLogger(os.path.splitext(os.path.basename(sys.argv[0]))[0])
 pp = pprint.PrettyPrinter(indent=2)
@@ -29,8 +36,15 @@ def parse_args(args=sys.argv[1:]):
         formatter_class=CustomFormatter)
 
     g = parser.add_argument_group("clim2influxdb settings")
-    g.add_argument("--date", action="store",
-        type=str, help="date like 20190925")
+    g.add_argument("--current", action="store_true",
+            default=False,
+            help="return current values")
+    g.add_argument("--day", metavar="N",
+            type=str,
+            help="date of the day to use. Ex 2019/08/08")
+    g.add_argument("--month", metavar="N",
+            type=str,
+            help="date of the month to use. Ex 2019/08/08")
 
     g = parser.add_mutually_exclusive_group()
     g.add_argument("--debug", "-d", action="store_true",
@@ -55,29 +69,71 @@ def setup_logging(options):
         root.addHandler(ch)
 
 
-def main(options):
-    LOGIN = os.environ['CONFORT_CLOUD_LOGIN']
-    PASSWORD = os.environ['CONFORT_CLOUD_PASSWORD']
+def date2int(year, month, day, hour):
+    dd = datetime.datetime(year, month, day, hour, 0, 0)
+    return dd.strftime("%s000000000")
 
-    if options.date:
-        logging.debug("Getting for date:", options.date)
 
-    session = pcomfortcloud.Session(LOGIN, PASSWORD)
+def get_device(login, password):
+    session = pcomfortcloud.Session(login, password)
     session.login()
 
     devices = session.get_devices()
     logger.debug(pp.pformat(devices))
 
-    device = devices[0]['id']
-    logger.debug("Using device: " + device)
+    device_id = devices[0]['id']
+    logger.debug("Using device: " + device_id)
 
-    if options.date:
-        logger.debug("Getting date for date: " + options.date)
-        values = session.history(device, "Day", options.date)
-        logger.debug(pp.pformat(values))
-        res = ""
-    else:
-        values = session.get_device(device)
+    return (session, device_id)
+
+
+def get_historic(date, mode, db_name):
+    (year, month, day) = date.split('/')
+    date = year + month + day
+    logger.debug("Using date: " + date)
+
+    (session, device_id) = get_device(LOGIN, PASSWORD)
+    values = session.history(device_id, mode, date)
+    # logger.debug(pp.pformat(values))
+
+    historyDataList = values['parameters']['historyDataList']
+    logger.debug(pp.pformat(historyDataList))
+
+    year = int(year)
+    month = int(month)
+    day = int(day)
+    hour = 0
+    for record in historyDataList:
+        if mode is 'Day':
+            hour = int(record['dataNumber'])
+        else:
+            day = int(record['dataNumber']) + 1
+
+        temperatureInside = record['averageInsideTemp']
+
+        # no or invalid data?
+        if temperatureInside == -255:
+            continue
+
+        temperatureOutside = record['averageOutsideTemp']
+        consigne = record['averageSettingTemp']
+        consumption = record['consumption']
+        res = "%s consigne=%f,inside=%f,consumption=%f" % (db_name, consigne,
+                temperatureInside, consumption)
+        if temperatureOutside != -255:
+            res += ",outside=%f" % temperatureOutside
+        res += " " + date2int(year, month, day, hour)
+        print(res)
+
+
+if __name__ == "__main__":
+    options = parse_args()
+    setup_logging(options)
+
+    if options.current:
+        logging.debug("Getting current values.")
+        (session, device_id) = get_device(LOGIN, PASSWORD)
+        values = session.get_device(device_id)
         logger.debug(pp.pformat(values))
 
         parameters = values['parameters']
@@ -89,18 +145,12 @@ def main(options):
         res = "Clim consigne=%f,inside=%d" % (consigne, temperatureInside)
         if parameters['power'] is pcomfortcloud.constants.Power.On:
             res += ",outside=%d,power=1" % temperatureOutside
-
-    return res
-
-
-if __name__ == "__main__":
-    options = parse_args()
-    setup_logging(options)
-
-    try:
-        logger.debug("Get air conditionning data")
-        res = main(options)
         print(res)
-    except Exception as e:
-        logger.exception("%s", e)
-        sys.exit(1)
+
+    if options.day:
+        logging.debug("Getting for day:", options.day)
+        get_historic(options.day, 'Day', 'Clim_history')
+
+    if options.month:
+        logging.debug("Getting for month:", options.month)
+        get_historic(options.month, 'Month', 'Clim_history_jour')
